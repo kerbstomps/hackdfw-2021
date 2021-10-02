346c65849b0d44bd21c8c7a82f58861eb77273d9
realprogrammer5000 <me@programmer5000.com>
2021-10-02 17:45:50 -0500

    Update takepicture.js

--- src/pages/takepicture.js
+++ src/pages/takepicture.js
@@ -10,7 +10,7 @@ import CardMedia from "@mui/material/CardMedia";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import Button from "@mui/material/Button";
 import {PhotoCamera} from "@mui/icons-material";
-import {Stack} from "@mui/material";
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Stack} from "@mui/material";
 import styled from "@emotion/styled";
 import {Component} from "react";
 import { navigate } from "gatsby"
@@ -58,16 +58,43 @@ class TakePicture extends Component {
         super(props);
 
         this.state = {
-            uploadedImage: undefined
+            uploadedImage: undefined,
+            isLoaded: false,
+            apiData: null,
+            error: null
         };
     }
 
+
+    componentDidMount(){
+        this.updateData();
+    }
+
+    async updateData() {
+        try {
+            const result = await (await fetch("https://poop")).json();
+
+            this.setState({
+                isLoaded: true,
+                apiData: result
+            });
+        }catch(error) {
+            this.setState({
+                isLoaded: true,
+                error
+            });
+        }
+    }
 
     render() {
         const word = "un zapato";
         const photographer = "Steve";
         const location = "Somewhere";
         const src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEsAAABLCAIAAAC3LO29AAABhGlDQ1BJQ0MgcHJvZmlsZQAAKJF9kT1Iw0AcxV9TRSkVBTuIOGSoThbELxy1CkWoEGqFVh1MLv2CJg1Jiouj4Fpw8GOx6uDirKuDqyAIfoC4uTkpukiJ/0sKLWI8OO7Hu3uPu3eAUC8zzeoYAzTdNlOJuJjJropdrwhBQB9imJKZZcxJUhK+4+seAb7exXiW/7k/R4+asxgQEIlnmWHaxBvE05u2wXmfOMKKskp8Tjxq0gWJH7muePzGueCywDMjZjo1TxwhFgttrLQxK5oa8SRxVNV0yhcyHquctzhr5Spr3pO/MJzTV5a5TnMICSxiCRJEKKiihDJs6qsEnRQLKdqP+/gHXb9ELoVcJTByLKACDbLrB/+D391a+YlxLykcBzpfHOdjGOjaBRo1x/k+dpzGCRB8Bq70lr9SB2Y+Sa+1tOgR0LsNXFy3NGUPuNwBBp4M2ZRdKUhTyOeB9zP6pizQfwuE1rzemvs4fQDS1FXyBjg4BEYKlL3u8+7u9t7+PdPs7wenInK8muCaHAAAAAlwSFlzAAAuIwAALiMBeKU/dgAAAAd0SU1FB+UKAhAxOwGUvV0AAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAAAXElEQVR42u3PAQ0AAAgDoGv/ztrjgwbMpdzG0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQsM8Dn84BlefWENIAAAAASUVORK5CYII=";
+
+        const {error, isLoaded} = this.state;
+
+        let isOpen = true;
 
         const imageUploaded = async (e) => {
             const elem = e.target;
@@ -79,40 +106,71 @@ class TakePicture extends Component {
             }
         };
 
-        return (<Layout>
-            <Seo title="Take a picture!"/>
-            <h1>Look around you for</h1>
-            <blockquote id="word">{word}</blockquote>
+        const handleClose = (e) => {
+            console.log(e);
+            this.updateData();
+        };
 
-            <Stack direction="row" spacing={1}>
-                <Card sx={{maxWidth: 300}} className="card">
-                    <CardMedia
-                        component="img"
-                        image={src}
-                        alt="user image"
-                        className="card-media"
-                    />
-                    <CardContent>
-                        <Stack alignItems="center"
-                               direction="row" spacing={1}>
-                            <LocationOnIcon/><Typography variant="body2" color="text.secondary">By {photographer} in {location}</Typography>
-                        </Stack>
-                    </CardContent>
-                </Card>
+        if(error) {
+            return (<Dialog
+                open={isOpen}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"An unexpected error occurred"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {error.stack.toString()}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} autoFocus>
+                        Retry
+                    </Button>
+                </DialogActions>
+            </Dialog>);
+        }else if(isLoaded) {
+            return (<Layout>
+                <Seo title="Take a picture!"/>
+                <h1>Look around you for</h1>
+                <blockquote id="word">{word}</blockquote>
 
-                <div>
-                    <label htmlFor="icon-button-file">
-                        <Input onChange={imageUploaded} accept="image/*" id="icon-button-file" type="file"/>
-                        <Button color="primary" aria-label="upload picture" component="span" variant="contained"
-                                size="large" startIcon={<PhotoCamera/>}>
-                            Take or upload photo
-                        </Button>
-                    </label>
+                <Stack direction="row" spacing={1}>
+                    <Card sx={{maxWidth: 300}} className="card">
+                        <CardMedia
+                            component="img"
+                            image={src}
+                            alt="user image"
+                            className="card-media"
+                        />
+                        <CardContent>
+                            <Stack alignItems="center"
+                                   direction="row" spacing={1}>
+                                <LocationOnIcon/><Typography variant="body2"
+                                                             color="text.secondary">By {photographer} in {location}</Typography>
+                            </Stack>
+                        </CardContent>
+                    </Card>
 
-                    { this.state.uploadedImage && <UploadedPicture uploaded={this.state.uploadedImage}/>}
-                </div>
-            </Stack>
-        </Layout>);
+                    <div>
+                        <label htmlFor="icon-button-file">
+                            <Input onChange={imageUploaded} accept="image/*" id="icon-button-file" type="file"/>
+                            <Button color="primary" aria-label="upload picture" component="span" variant="contained"
+                                    size="large" startIcon={<PhotoCamera/>}>
+                                Take or upload photo
+                            </Button>
+                        </label>
+
+                        {this.state.uploadedImage && <UploadedPicture uploaded={this.state.uploadedImage}/>}
+                    </div>
+                </Stack>
+            </Layout>);
+        }else{
+            return (<div>Loading...</div>);
+        }
     }
 }
 
