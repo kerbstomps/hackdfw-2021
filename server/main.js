3a22be691896161b3b81dd48fe5fe37e51c01a48
heartgg <a.szumski98@gmail.com>
2021-10-02 13:38:03 -0500

    atlas connection

--- server/main.js
+++ server/main.js
@@ -11,25 +11,4 @@ app.use(express.json());
 const endpoints = require('./endpoints.js');
 endpoints(app);
 
-const dbClient = new MongoClient(process.env.DATABASE_URI);
-
-async function test (message) {
-    try {
-        await dbClient.connect();
-        const db = dbClient.db('test');
-        const collection = db.collection(message);
-        let data = {
-            'sender': 'test',
-            'message': 'test'
-        }
-        const document = await collection.insertOne(data);
-        return `${document.insertedId}`;
-    } catch (error) {
-        console.error(error);
-        return `ERROR: ${error}`;
-    } finally {
-        await dbClient.close();
-    }
-}
-
-test('hi');
+const client = new MongoClient(process.env.DATABASE_URI);
