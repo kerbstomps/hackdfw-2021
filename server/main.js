5d306c50e54b5bf4efc40b2fe8da25cba7d43d6a
heartgg <a.szumski98@gmail.com>
2021-10-02 11:28:37 -0500

    test db

--- server/main.js
+++ server/main.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
@@ -8,3 +11,25 @@ app.use(express.json());
 const endpoints = require('./endpoints.js');
 endpoints(app);
 
+const dbClient = new MongoClient(process.env.DATABASE_URI);
+
+async function test (message) {
+    try {
+        await dbClient.connect();
+        const db = dbClient.db('test');
+        const collection = db.collection(message);
+        let data = {
+            'sender': 'test',
+            'message': 'test'
+        }
+        const document = await collection.insertOne(data);
+        return `${document.insertedId}`;
+    } catch (error) {
+        console.error(error);
+        return `ERROR: ${error}`;
+    } finally {
+        await dbClient.close();
+    }
+}
+
+test('hi');
