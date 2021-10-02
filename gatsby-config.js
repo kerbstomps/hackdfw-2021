9fa8c3cd3aad7b2310d613618533cba9d3d51528
realprogrammer5000 <me@programmer5000.com>
2021-10-01 23:55:25 -0500

    Add gatsby to s3 integration (deploy with npm run deploy)

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "hackdfw-worldpicture",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
