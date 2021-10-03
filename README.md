b1688181d07d00bcaf7f9b41f3d9de6211c68eeb
kerbstomps <kerbykaska@gmail.com>
2021-10-02 19:41:39 -0500

    Added languages data

--- README.md
+++ README.md
@@ -8,6 +8,11 @@
   Gatsby's default starter
 </h1>
 
+## Resources Used:
+https://github.com/yanivam/react-svg-worldmap
+https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes
+
+
 Kick off your project with this default boilerplate. This starter ships with the main Gatsby configuration files you might need to get up and running blazing fast with the blazing fast app generator for React.
 
 _Have another more specific idea? You may want to check out our vibrant collection of [official and community-created starters](https://www.gatsbyjs.com/docs/gatsby-starters/)._
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,36 +3,15 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import WorldMap from 'react-svg-worldmap';
+// import WorldMap from 'react-svg-worldmap';
 
-const data = [
-  // {country: 'cn', value: 1389618778}, // china
-  // {country: 'in', value: 1311559204}, // india
-  {country: 'us', value: 1}, // united states
-  // {country: 'id', value: 264935824}, // indonesia
-  // {country: 'pk', value: 210797836}, // pakistan
-  // {country: 'br', value: 210301591}, // brazil
-  // {country: 'ng', value: 208679114}, // nigeria
-  // {country: 'bd', value: 161062905}, // bangladesh
-  // {country: 'ru', value: 141944641}, // russia
-  {country: 'mx', value: 1}, // mexico
-];
-
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    {/* <h1>hello</h1> */}
-    <WorldMap
-        color="red"
-        title="Top 10 Populous Countries"
-        value-suffix="people"
-        data={data}
-      />
-    {/* <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p> */}
-  </Layout>
-)
-
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Seo title="Home" />
+      <h1>hello world</h1>
+    </Layout>
+  )
+}
+  
 export default IndexPage
