b8b5e74472794411959a9ef54d29798d2be03e15
realprogrammer5000 <me@programmer5000.com>
2021-10-03 00:06:35 -0500

    Footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
