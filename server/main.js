835cef3e2b68c528236ac09f8aeaf47e788bd20b
heartgg <a.szumski98@gmail.com>
2021-10-02 20:29:37 -0500

    Actually increase request size

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 require('dotenv').config()
 
 const app = express();
@@ -9,7 +10,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(cors());
-app.use(express.bodyParser({limit: '50mb'}));
+app.use(bodyParser.json({ type: 'application/json' , limit: '50mb'}));
 
 app.listen(3000);
 
