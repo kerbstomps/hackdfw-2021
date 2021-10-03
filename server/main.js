8c18bbb80845fd62c5a418f36d4976e8c12ae901
heartgg <a.szumski98@gmail.com>
2021-10-02 19:20:20 -0500

    cors

--- server/main.js
+++ server/main.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+var cors = require('cors');
 require('dotenv').config()
 
 const app = express();
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
+app.use(cors());
 app.listen(3000);
 
 // register endpoints
